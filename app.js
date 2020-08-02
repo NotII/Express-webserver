122ae0aa71f638e14fc40bd8861e19ba1c7d24e3
NotII <gw10lindengerrard@glow.sch.uk>
2020-08-02 17:54:10 +0100

    Remove files

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const responseTime = require("response-time");
 app.use(responseTime());
 var fs = require("fs");
 var request = require("request");
+const config = require("./config");
+const messages = config.responses.messages;
 app.use(express.static("./actions"));
 // --------------------------------------------- \\
 app.get("/", (req, res) => {
@@ -263,6 +265,149 @@ function handleUpload(req, res) {
     res.send("No file uploaded.");
   }
 }
+
+function genId(length, isFile, ext) {
+  var id = "";
+  const charset = config.charset; //Characters that can be used to generate ids
+
+  for (var i = 0; i < length; i++)
+    id += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  //Check if id is blacklisted
+  if (config.blacklistedIds.includes(id)) return genId(length);
+
+  //Checks based on generating id for files or urls
+  if (isFile) {
+    //Check if file already exists
+    fs.exists(
+      __dirname + path.sep + "images" + path.sep + id + "." + ext,
+      (exists) => {
+        if (exists) return genId(length, true, ext);
+      }
+    );
+  } else {
+    //Check if id already exists if generating for url shortener
+    mysqlConnection.query(
+      "SELECT * FROM ids WHERE id = ?",
+      [id],
+      (err, data, fields) => {
+        if (data[0]) {
+          if (data[0].code == id) return genId(length);
+        }
+      }
+    );
+  }
+  return id;
+}
+
+function handleGetId(req, res) {
+  const id = req.params.id;
+
+  const file = __dirname + path.sep + "images" + path.sep + id;
+
+  fs.exists(file, (exists) => {
+    console.log(file, exists);
+    if (exists) {
+      res.sendFile(file);
+    } else {
+      mysqlConnection.query(
+        "SELECT * FROM ids WHERE id = ?",
+        [id],
+        (err, result, fields) => {
+          if (err == null) {
+            if (result[0]) {
+              if (result[0].id == id) {
+                if (
+                  !result[0].url.startsWith("http") &&
+                  !result[0].url.startsWith("ftp")
+                )
+                  res.redirect("http://" + result[0].url);
+                else res.redirect(result[0].url);
+              } else res.redirect("/404");
+            } else res.redirect("/404");
+          } else {
+            res.status(500).json({
+              message: messages.unknown_error,
+              error: errors.unknown_error,
+              success: false,
+            });
+            console.log(err);
+          }
+        }
+      );
+    }
+  });
+}
+
+app.use(
+  fileUpload({
+    safeFileNames: false,
+    preserveExtension: true,
+    limits: {
+      fileSize: config.fileSizeLimit,
+    },
+  })
+);
+app.set("view engine", "pug");
+app.get("/sharex", (req, res) => {
+  let _config = `{
+  "Version": "12.4.1",
+  "DestinationType": "ImageUploader, TextUploader, FileUploader",
+  "RequestMethod": "POST",
+  "RequestURL": "http://${req.host}/up.php",
+  "Body": "MultipartFormData",
+  "FileFormName": "sharex"
+  }`;
+  res.set("Content-Disposition", "attachment;filename=" + req.host + ".sxcu");
+  res.set("Content-Type", "application/octet-stream");
+  res.send(_config);
+});
+app.post("/up.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/upload.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/up", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/upload", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/u.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/u", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/up.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/upload.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/up", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/upload", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/u.php", (req, res) => {
+  return handleUpload(req, res);
+});
+app.post("/i/u", (req, res) => {
+  return handleUpload(req, res);
+});
+app.get("/:image", (req, res) => {
+  fs.readFile(`./images/${req.params.image}`, function (err, data) {
+    if (err) {
+      res.send("404 Not Found");
+      return;
+    }
+    res.write(data);
+    return res.end();
+  });
+});
 
 app.listen(80);
 const https = require("https");
